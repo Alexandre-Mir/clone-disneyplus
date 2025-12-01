9b0a0307020fcac086ac9affcec91f08a72cc464
Alexandre-Mir <110476249+Alexandre-Mir@users.noreply.github.com>
2025-12-01 20:25:17 -0300

    Elemento botão de "entrar"

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,44 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
-const sourcemaps = require("gulp-sourcemaps")
+const sourcemaps = require("gulp-sourcemaps");
+
+const imagemin = require("gulp-imagemin");
+const webp = require("imagemin-webp");
+const changed = require("gulp-changed");
+
+const paths = {
+	styles: {
+		src: "./src/styles/*.scss",
+		dest: "./dist/css",
+	},
+	images: {
+		src: "./src/images/**/*.{jpg,jpeg,png,gif,webp}",
+		dest: "./dist/images",
+	},
+};
 
 function styles() {
 	return gulp
-		.src(".src/styles/*.scss")
-    .pipe(sourcemaps.init())
+		.src(paths.styles.src)
+		.pipe(sourcemaps.init())
 		.pipe(sass({ outputStyle: "compressed" }))
 		.on("error", sass.logError)
-    .pipe(sourcemaps.write('./maps'))
-		.pipe(gulp.dest("./dist/css"));
+		.pipe(sourcemaps.write("./maps"))
+		.pipe(gulp.dest(paths.styles.dest));
+}
+
+function optimizeImages() {
+	return gulp
+		.src(paths.images.src)
+		.pipe(changed(paths.images.dest))
+		.pipe(imagemin([webp({ quality: 75 })], { verbose: true }))
+		.pipe(gulp.dest(paths.images.dest));
 }
 
 exports.default = function () {
 	styles();
-
-	gulp.watch(".src/styles/*.scss", styles);
+	gulp.watch(paths.styles.src, styles);
+	gulp.watch(paths.images.src, optimizeImages);
 };
+
+exports.images = optimizeImages;
