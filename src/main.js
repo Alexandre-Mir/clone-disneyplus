3838f0760b6b725d781020dfe25cc3b4635eb57d
Alexandre-Mir <xandenextel@gmail.com>
2026-01-12 03:05:14 -0300

    feat: implementar seções de FAQ e Rodapé
    - Adicionada funcionalidade de FAQ (acordeão) com JavaScript.
    - Implementado estilo SCSS para o FAQ e Rodapé.
    - Atualizado o index.html com as novas seções de FAQ e Footer.
    - Ajustada a lógica de abas no main.js para suportar as novas interações.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 const buttons = document.querySelectorAll("[data-tab-button]");
+const questions = document.querySelectorAll("[data-faq-question]");
 
 document.addEventListener("DOMContentLoaded", () => {
 	buttons.forEach((button) => {
@@ -11,7 +12,18 @@ document.addEventListener("DOMContentLoaded", () => {
 			button.classList.add("shows__tabs__button--is-active");
 		});
 	});
+
+	questions.forEach((question) => {
+		question.addEventListener("click", toggleAnswer)
+	})
 });
+
+function toggleAnswer(elemento) {
+	const classe = 'faq__questions__item--is-open'
+	const elementoPai = elemento.target.parentNode
+
+	elementoPai.classList.toggle(classe)
+}
 
 function removeActiveButton() {
 	buttons.forEach((button) => {
