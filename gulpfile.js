c0d0ca495dfb277b0c3405e86d5ebddc0f191f8e
Alexandre-Mir <xandenextel@gmail.com>
2026-01-12 16:00:24 -0300

    configurando o uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
+const uglify = require("gulp-uglify");
 
 const imagemin = require("gulp-imagemin");
 const changed = require("gulp-changed");
@@ -14,7 +15,17 @@ const paths = {
 		src: "./src/images/**/*.{jpg,jpeg,png,gif,webp,svg}",
 		dest: "./dist/images",
 	},
+	scripts: {
+		src: "./src/js/*.js",
+		dest: "./dist/js",
+	},
 };
+
+function scripts() {
+	return gulp.src(paths.scripts.src)
+		.pipe(uglify())
+		.pipe(gulp.dest(paths.scripts.dest));
+}
 
 function styles() {
 	return gulp
@@ -43,8 +54,9 @@ function optimizeImages() {
 exports.default = function () {
 	gulp.watch(paths.styles.src, styles);
 	gulp.watch(paths.images.src, optimizeImages);
+	gulp.watch(paths.scripts.src, scripts);
 };
 
 exports.watch = exports.default;
-exports.build = gulp.parallel(styles, optimizeImages);
+exports.build = gulp.parallel(styles, optimizeImages, scripts);
 exports.images = optimizeImages;
